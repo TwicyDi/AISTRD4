e7080e3a3aa9c130542a368fc58d645c1d4ef2bd
TwicyDi <48177823+TwicyDi@users.noreply.github.com>
2019-05-13 14:24:54 +0300

    Update BF.h

--- Bellman-Ford/main/BF.h
+++ Bellman-Ford/main/BF.h
@@ -8,4 +8,3 @@ using namespace std;
 void read_file(map<string>*);
 int** create_graph_matrix(map<string>);
 string find_way(int**, size_t, int, int, int&);
-void showing_the_best_way(string path, map<string> city);
